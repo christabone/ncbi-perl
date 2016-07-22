767bdcb3b056a9446754fafb65fd585b570ab9b6
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-22 15:33:34 -0400

    More progress on XML parsing.

--- sra_xml_parser.pl
+++ sra_xml_parser.pl
@@ -3,6 +3,8 @@
 #A script to parse the XML fields of sra.xml files obtained from NCBI.
 #Derived from the processing_from_ncbi.pl script used in-house at FlyBase.
 #Version 0.1
+#
+#Built for SRA XSD 1-6a: http://www.ncbi.nlm.nih.gov/viewvc/v1/trunk/sra/doc/SRA_1-6a/
 
 use warnings;
 use strict;
@@ -16,7 +18,7 @@ my $OUTFILE = "output.log";
 open(STDERR, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
 open(STDOUT, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
 
-# Reading information from SRA files in SRA directory.
+# Reading information from SRA files in SRA directory. We're looking for the directory "SRA" containing a list of XML files.
 my $sra_directory = "SRA";
 opendir SRADIR, $sra_directory or die "Can't open SRA directory: $!";
 my @SRAfiles = grep {$_ ne '.' && $_ ne '..' } readdir SRADIR; # Read the SRA file names into an array.
@@ -29,7 +31,10 @@ foreach my $filename (@SRAfiles) {
 	foreach my $MainNodes ($sra_xml->findnodes('//EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE')) {
 		foreach my $ExperimentNode ($MainNodes->findnodes('./EXPERIMENT')) {
 			my $accession = $ExperimentNode->getAttribute('accession');
-			my $alias = $ExperimentNode->getAttribute('alias');
+        	if(!($accession)) {
+            	print "WARNING: No $accession found for $alias!\n";
+        	}
+        	my $alias = $ExperimentNode->getAttribute('alias');
 			$main_hash{$accession}{alias} = $alias; # Assigning values to a two dimensional hash. 
 		}
 	}
