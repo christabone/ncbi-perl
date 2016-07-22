b43f507b9074564b7361500856d57be90049d442
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-22 14:01:38 -0400

    Simple XML parsing added, more in progress.

--- sra_xml_parser.pl
+++ sra_xml_parser.pl
@@ -6,8 +6,11 @@
 
 use warnings;
 use strict;
+use Carp;
 use Data::Dumper;
 use XML::LibXML;
+
+my %main_hash; # Hash to store all parsed XML data.
 
 my $OUTFILE = "output.log";
 open(STDERR, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
@@ -19,8 +22,17 @@ opendir SRADIR, $sra_directory or die "Can't open SRA directory: $!";
 my @SRAfiles = grep {$_ ne '.' && $_ ne '..' } readdir SRADIR; # Read the SRA file names into an array.
 closedir SRADIR;
 
-# Subroutine to parse SRA files.
-sub parse {
+foreach my $filename (@SRAfiles) {
 	my $parser = XML::LibXML->new("1.0","UTF-8");
-
+	my $dir_filename = "SRA/" . $filename; # Add the directory name.
+	my $sra_xml = $parser->parse_file($dir_filename) or die "Cannot parse document: $@";
+	foreach my $MainNodes ($sra_xml->findnodes('//EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE')) {
+		foreach my $ExperimentNode ($MainNodes->findnodes('./EXPERIMENT')) {
+			my $accession = $ExperimentNode->getAttribute('accession');
+			my $alias = $ExperimentNode->getAttribute('alias');
+			$main_hash{$accession}{alias} = $alias; # Assigning values to a two dimensional hash. 
+		}
+	}
 }
+
+print Data::Dumper->Dump([\%main_hash], ['*main_hash']);
