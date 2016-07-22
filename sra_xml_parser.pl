94e5009c4938cc1b0090720a924907d8ae3ff165
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-22 11:19:41 -0400

    Added the very humble beginnings of the SRA XML parser script.

---
+++ sra_xml_parser.pl
@@ -0,0 +1,10 @@
+#!usr/bin/perl
+#sra_xml_parser.pl
+#A script to parse the XML fields of sra.xml files obtained from NCBI.
+#Derived from the processing_from_ncbi.pl script used in-house at FlyBase.
+#Version 0.1
+
+use warnings;
+use strict;
+use Data::Dumper;
+
