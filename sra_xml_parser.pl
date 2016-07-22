b01fb9c4c412d828674489fa3837bea694ecd102
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-22 13:05:37 -0400

    More code for XML parsing added.

--- sra_xml_parser.pl
+++ sra_xml_parser.pl
@@ -7,5 +7,20 @@
 use warnings;
 use strict;
 use Data::Dumper;
+use XML::LibXML;
 
-# Test edit of branches.
+my $OUTFILE = "output.log";
+open(STDERR, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
+open(STDOUT, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
+
+# Reading information from SRA files in SRA directory.
+my $sra_directory = "SRA";
+opendir SRADIR, $sra_directory or die "Can't open SRA directory: $!";
+my @SRAfiles = grep {$_ ne '.' && $_ ne '..' } readdir SRADIR; # Read the SRA file names into an array.
+closedir SRADIR;
+
+# Subroutine to parse SRA files.
+sub parse {
+	my $parser = XML::LibXML->new("1.0","UTF-8");
+
+}
