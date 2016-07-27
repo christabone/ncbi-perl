a45a29bf3d668c6591ae482278ed9c1cbb5ca9e9
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-27 16:39:17 -0400

    Basic sample extraction added.

--- sra_xml_parser.pl
+++ sra_xml_parser.pl
@@ -2,41 +2,58 @@
 #sra_xml_parser.pl
 #A script to parse the XML fields of sra.xml files obtained from NCBI.
 #Derived from the processing_from_ncbi.pl script used in-house at FlyBase.
-#Version 0.1
 #
 #Built for SRA XSD 1-6a: http://www.ncbi.nlm.nih.gov/viewvc/v1/trunk/sra/doc/SRA_1-6a/
-
+#
+#Nodes and data retrieved:
+#//EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE
+#---->/EXPERIMENT::accession (extracted)
+#---->/EXPERIMENT::alias (extracted)
+#---->/SAMPLE/SAMPLE_ATTRIBUTES
+#-------->/SAMPLE_ATTRIBUTE
+#------------>/TAG (extracted)
+#------------>/VALUE (extracted)
 use warnings;
 use strict;
 use Carp;
 use Data::Dumper;
 use XML::LibXML;
 
+use version; our $VERSION = qv(0.0.1);
+
 my %main_hash; # Hash to store all parsed XML data.
 
-my $OUTFILE = "output.log";
+my $OUTFILE = 'output.log';
 open(STDERR, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
 open(STDOUT, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
 
 # Reading information from SRA files in SRA directory. We're looking for the directory "SRA" containing a list of XML files.
-my $sra_directory = "SRA";
-opendir SRADIR, $sra_directory or die "Can't open SRA directory: $!";
-my @SRAfiles = grep {$_ ne '.' && $_ ne '..' } readdir SRADIR; # Read the SRA file names into an array.
+my $sra_directory = 'SRA';
+opendir SRADIR, $sra_directory or croak "Can't open SRA directory: $!";
+my @SRAfiles = grep {$_ ne q{.} && $_ ne q{..} } readdir SRADIR; # Read the SRA file names into an array.
 closedir SRADIR;
 
 foreach my $filename (@SRAfiles) {
-	my $parser = XML::LibXML->new("1.0","UTF-8");
-	my $dir_filename = "SRA/" . $filename; # Add the directory name.
-	my $sra_xml = $parser->parse_file($dir_filename) or die "Cannot parse document: $@";
-	foreach my $MainNodes ($sra_xml->findnodes('//EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE')) {
-		foreach my $ExperimentNode ($MainNodes->findnodes('./EXPERIMENT')) {
-			my $accession = $ExperimentNode->getAttribute('accession');
+	my $parser = XML::LibXML->new('1.0','UTF-8');
+	my $dir_filename = "SRA/$filename"; # Add the directory name.
+	my $sra_xml = $parser->parse_file($dir_filename) or croak "Cannot parse document: $@";
+	foreach my $main_nodes ($sra_xml->findnodes('//EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE')) {
+		my $accession = '';
+		foreach my $experiment_nodes ($main_nodes->findnodes('./EXPERIMENT')) {
+			$accession = $experiment_nodes->getAttribute('accession');
+			my $alias = $experiment_nodes->getAttribute('alias');
         	if(!($accession)) {
             	print "WARNING: No $accession found for $alias!\n";
         	}
-        	my $alias = $ExperimentNode->getAttribute('alias');
-			$main_hash{$accession}{alias} = $alias; # Assigning values to a two dimensional hash. 
+			$main_hash{$accession}{alias} = $alias; # Assigning values to a two dimensional hash.
 		}
+	    foreach my $sample_attributes_node ($main_nodes->findnodes('./SAMPLE/SAMPLE_ATTRIBUTES')) {
+	    	foreach my $individual_sample ($sample_attributes_node->findnodes('./SAMPLE_ATTRIBUTE')) {
+	    	my $tag = $individual_sample->findvalue('./TAG');
+	    	my $value = $individual_sample->findvalue('./VALUE');
+	    	$main_hash{$accession}{$tag} = $value;
+	    	}
+	    }
 	}
 }
 
