7812c77962ad2df73de04e4143c947dc9eac4cff
Chris Tabone <ctabone@morgan.harvard.edu>
2016-07-28 12:54:38 -0400

    Added curator.pl

---
+++ sra_curator.pl
@@ -0,0 +1,25 @@
+#!usr/bin/perl
+#sra_curator.pl
+#A script to curate fields derived from SRA entries obtained from NCBI.
+#Derived from the processing_from_ncbi.pl script used in-house at FlyBase.
+#
+#TO USE: This script will look for the file "extracted.results" from the script sra_xml_parser.pl in the directory where it is placed.
+#
+use warnings;
+use strict;
+use Carp;
+use Data::Dumper;
+use Storable;
+use OBO::Parser::OBOParser;
+
+use version; our $VERSION = qv(0.0.1);
+
+my $OUTFILE = 'output_curator.log';
+open(STDERR, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
+open(STDOUT, "| tee -i $OUTFILE") or die "WARNING: ERROR: Cannot open output.log\n";
+
+my %main_hash = %{retrieve('extracted.results')}; # Load the extracted.results file from the sra_xml_parser.pl script.
+
+
+
+# print Data::Dumper->Dump([\%main_hash], ['*main_hash']);
